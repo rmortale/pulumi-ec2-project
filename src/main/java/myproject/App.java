446c29a371fb045151308aec20bdbfea5703d416
Antonio Dolce <dolce.antonio@gmail.com>
2024-08-09 18:21:07 +0200

    get default vpc

--- src/main/java/myproject/App.java
+++ src/main/java/myproject/App.java
@@ -1,14 +1,19 @@
 package myproject;
 
 import com.pulumi.Pulumi;
-import com.pulumi.core.Output;
+import com.pulumi.aws.ec2.DefaultVpc;
 import com.pulumi.aws.s3.Bucket;
 
 public class App {
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
+
+            // Get the default VPC for the current region.
+            var vpc = new DefaultVpc("default-vpc");
+
             var bucket = new Bucket("my-bucket");
             ctx.export("bucketName", bucket.bucket());
+            ctx.export("defaultVpc", vpc.arn());
         });
     }
 }
